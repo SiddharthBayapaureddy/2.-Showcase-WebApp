b13e0b7a84c9c389344611263dcba115f08ae8cc
Siddharth <siddharthbayapureddy@gmail.com>
2024-11-01 23:09:05 +0530

    URL changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas 
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/pfp.png")
+    st.image("images/pfp.png", width=500  )
 
 with col2:
     st.title('Siddharth')
@@ -23,14 +23,14 @@ st.write(content)
 
 df = pandas.read_csv('data.csv' , sep=';')
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
         st.info(row['description'])
         photo = f'images/{row['image']}'
-        print(photo)
         st.image(photo)
+        st.write(f"[Source Code]({row['url']})")
        
         
 
@@ -41,5 +41,5 @@ with col4:
         st.header(row['title'])
         st.info(row['description'])
         photo = f'images/{row['image']}'
-        print(photo)
         st.image(photo)
+        st.write(f"[Source Code]({row['url']})")
